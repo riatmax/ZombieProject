8637aac8d587ad12bc89c3fcb8df34e5bc6704e1
riatmax <riatmax@gmail.com>
2025-03-26 15:17:53 -0400

    Initial commit

---
+++ Source/ZombieProject/ZombieProject.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ZombieProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ZombieProject, "ZombieProject" );
+
+DEFINE_LOG_CATEGORY(LogZombieProject)
+ 
---
+++ Source/ZombieProject/ZombieProjectGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ZombieProjectGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AZombieProjectGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AZombieProjectGameMode();
+};
+
+
+
